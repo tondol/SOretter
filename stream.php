29185570fa2b21811c99de3dd95dcdd4f02dd3e8
tondol <tondol@gmail.com>
2015-01-04 05:15:54 +0900

    他人へのmentionでもmentionとして色分けされてしまうバグを修正

--- stream.php
+++ stream.php
@@ -22,7 +22,12 @@ header('Transfer-Encoding', 'chunked');
 header('Content-Type', 'application/octet-stream');
 ob_flush(); flush();
 
-$connection->stream('https://userstream.twitter.com/1.1/user.json', array(), function ($response) {
+$user = $connection->get('account/verify_credentials');
+
+$connection->stream('https://userstream.twitter.com/1.1/user.json', array(), function ($response) use ($user) {
+	if (isset($response->in_reply_to_user_id) && $response->in_reply_to_user_id == $user->id) {
+		$response = (object)array_merge((array)$response, array('is_mention' => true));
+	}
 	echo json_encode($response) . "\r\n";
 	ob_flush(); flush();
 	return true;
